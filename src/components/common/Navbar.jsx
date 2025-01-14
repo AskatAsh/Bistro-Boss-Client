1e4057baa1568d6174635b77355578b77d959575
AskatAsh <abdulaskat@gmail.com>
2025-01-14 21:07:33 +0600

    added changes responsive style in navbar using tailwind css

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -9,7 +9,7 @@ import {
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isLoggedIn] = useState(true);
 
   const navlinks = (
     <>
@@ -35,37 +35,38 @@ const Navbar = () => {
   );
 
   return (
-    <header className="fixed top-0 left-0 w-full z-50 bg-[rgba(21,21,21,0.5)] backdrop-blur-md text-white">
+    <header className="fixed top-0 left-0 w-full z-50 bg-[rgba(21,21,21,0.35)] text-white">
       <div className="max-w-7xl mx-auto flex items-center justify-between px-4 py-3">
         {/* Logo */}
         <div className="text-2xl font-semibold">
-          <a href="/" className="text-text">
-            BrandLogo
+          <a href="/" className="text-text font-cinzel flex flex-col">
+            <span className="font-black text-base sm:text-xl lg:text-2xl tracking-wider">BISTRO BOSS</span>
+            <span className="text-sm sm:text-lg lg:text-xl tracking-[0.25em] md:tracking-[0.3em]">RESTAURANT</span>
           </a>
         </div>
 
         <div className="flex items-center justify-end gap-4">
           {/* Desktop Nav Links */}
-          <nav className="hidden md:flex space-x-6">{navlinks}</nav>
+          <nav className="hidden md:flex gap-6 lg:gap-8 uppercase font-medium">{navlinks}</nav>
 
           {/* User Actions */}
           <div className="flex items-center space-x-4">
             {isLoggedIn ? (
               <>
                 <span className="flex items-center gap-1">
-                  <span className="max-[500px]:hidden">Our Shop </span>
+                  <span className="max-[500px]:hidden uppercase font-medium">Our Shop </span>
                   <FaShoppingCart className="text-lg cursor-pointer hover:text-accent" />
                 </span>
                 <span className="flex items-center gap-1">
-                  <span className="max-[500px]:hidden">Sign Out </span>
+                  <span className="max-[500px]:hidden uppercase font-medium">Sign Out </span>
                   <FaSignOutAlt className="text-xl cursor-pointer hover:text-accent" />
                 </span>
-                <FaUserCircle className="text-xl cursor-pointer hover:text-accent" />
+                <FaUserCircle className="text-2xl cursor-pointer hover:text-accent" />
               </>
             ) : (
               <a
                 href="#login"
-                className="bg-[rgba(238,255,37,1)] text-black px-4 py-2 rounded-md hover:bg-yellow-300"
+                className="bg-[rgba(238,255,37,1)] text-black px-4 py-2 rounded-md hover:bg-yellow-300 uppercase font-medium"
               >
                 Login
               </a>
