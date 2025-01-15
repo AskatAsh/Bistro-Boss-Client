0a8876a4525aa9b4f68e488da7dc727677914d15
AskatAsh <abdulaskat@gmail.com>
2025-01-15 15:35:44 +0600

    applied react-responsive-carousel default lazy loading to all images

--- src/pages/Home/sections/HeroBanner.jsx
+++ src/pages/Home/sections/HeroBanner.jsx
@@ -1,7 +1,7 @@
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import images from '../../../../src/constants'
-import '../../../../src/styles/heroBanner.css'
+import images from "../../../../src/constants";
+import "../../../../src/styles/heroBanner.css";
 
 const HeroBanner = () => {
   return (
@@ -13,15 +13,18 @@ const HeroBanner = () => {
         infiniteLoop={true}
         className="h-full overflow-hidden"
         dynamicHeight={true}
-        style={{height: "700px", border: "2px solid red"}}
+        lazyLoad={true}
       >
-        {
-          images.map((image, idx) =>
+        {images.map((image, idx) => (
           <div key={idx} className="h-full">
-          <img src={image} className="w-full h-full object-cover" alt={`image of slide ${idx+1}`} />
-        </div>
-          )
-        }
+            <img
+              src={image}
+              className="w-full h-full object-cover"
+              alt={`image of slide ${idx + 1}`}
+              loading="lazy"
+            />
+          </div>
+        ))}
       </Carousel>
     </section>
   );
