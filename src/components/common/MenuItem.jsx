357b610938b40464024a343212d0c5c1e06ca6b7
AskatAsh <abdulaskat@gmail.com>
2025-01-16 19:26:02 +0600

    added small changes in image and text styling using tailwind css

--- src/components/common/MenuItem.jsx
+++ src/components/common/MenuItem.jsx
@@ -3,15 +3,15 @@ import { PropTypes } from "prop-types";
 const MenuItem = ({ item }) => {
   const { name, image, price, recipe } = item;
   return (
-    <div className="flex space-x-2">
+    <div className="flex items-start space-x-2">
       <img
         style={{ borderRadius: "0 200px 200px 200px" }}
-        className="w-[100px]"
+        className="w-24 h-24 object-cover"
         src={image}
         alt={`image of a ${recipe} recipe`}
       />
       <div>
-        <h3 className="uppercase">{name}----------</h3>
+        <h3 className="uppercase mb-2">{name} ----------</h3>
         <p>{recipe}</p>
       </div>
       <p className="text-yellow-500">${price}</p>
