5504c556408dfeaedf0d39353caf8564afd69ea7
AskatAsh <abdulaskat@gmail.com>
2025-01-16 18:57:35 +0600

    created menuItem common component and used in popularMenu section

--- src/pages/Home/sections/PopularMenu.jsx
+++ src/pages/Home/sections/PopularMenu.jsx
@@ -1,14 +1,24 @@
+import MenuItem from "../../../components/common/MenuItem";
 import useGetMenu from "../../../Hooks/useGetMenu";
-import SectionTitle from './../../../components/common/SectionTitle';
+import SectionTitle from "./../../../components/common/SectionTitle";
 
 const PopularMenu = () => {
-    const [menu] = useGetMenu();
-    console.log(menu);
-    return (
-        <section className="max-w-[1320px] w-11/12 mx-auto">
-            <SectionTitle title="From our Menu" subtitle="Check it out" />
-        </section>
-    );
+  const [menu] = useGetMenu();
+  const popular = menu.slice(0, 6);
+
+  return (
+    <section className="max-w-[1320px] w-11/12 mx-auto">
+      <SectionTitle title="From our Menu" subtitle="Check it out" />
+      <div className="grid md:grid-cols-2 gap-10">
+        {popular.map((item) => (
+          <MenuItem key={item._id} item={item}></MenuItem>
+        ))}
+      </div>
+      <button className="btn btn-outline border-0 border-b-4 mt-4">
+        View Full Menu
+      </button>
+    </section>
+  );
 };
 
 export default PopularMenu;
