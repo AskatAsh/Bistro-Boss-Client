ac2435360e4992f917921e1a68d2e6fb1d1e74e4
AskatAsh <abdulaskat@gmail.com>
2025-01-18 01:59:26 +0600

    added chages in itemcard component with tailwind css

--- src/components/shop/ItemCard.jsx
+++ src/components/shop/ItemCard.jsx
@@ -1,17 +1,19 @@
 import Button from "../common/Button";
-import cardImage from "../../../src/assets/home/slide1.jpg";
-const ItemCard = () => {
+import { PropTypes } from 'prop-types';
+const ItemCard = ({item}) => {
+  const {image, name, recipe, price} = item;
   return (
-    <div className="bg-dark7">
+    <div className="bg-dark7 relative flex flex-col h-full">
+      <span className="absolute top-3 right-3 bg-footerbg2 font-semibold text-text px-3 py-1 text-sm">${price}</span>
       <img
         className="w-full max-h-[300px] object-cover"
-        src={cardImage}
-        alt={`Image of food`}
+        src={image}
+        alt={`Image of ${name}`}
       />
-      <div className="px-6 lg:px-10 py-4 lg:py-8">
-        <h3 className="font-semibold text-2xl text-center">Caeser Salad</h3>
-        <p className="text-dark3 pt-2 mb-6">
-          Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.
+      <div className="px-6 lg:px-10 py-4 lg:py-8 flex flex-col flex-1">
+        <h3 className="font-semibold text-2xl text-center">{name}</h3>
+        <p className="text-dark3 pt-2 pb-4 text-center flex-grow">
+          {recipe}
         </p>
         <Button
           btnText="Add to Cart"
@@ -21,5 +23,9 @@ const ItemCard = () => {
     </div>
   );
 };
+
+ItemCard.propTypes = {
+  item: PropTypes.object,
+}
 
 export default ItemCard;
