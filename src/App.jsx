316211d3d5bff9dae64d56b630583552f69786f4
AskatAsh <abdulaskat@gmail.com>
2025-01-19 21:06:11 +0600

    installed tanstack query and used in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,22 @@ import { RouterProvider } from "react-router-dom";
 import router from "./routes/router";
 import { HelmetProvider } from "react-helmet-async";
 import AuthProvider from "./providers/AuthProvider";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
       <AuthProvider>
-        <HelmetProvider>
-          <RouterProvider router={router}></RouterProvider>
-        </HelmetProvider>
+        <QueryClientProvider client={queryClient}>
+          <HelmetProvider>
+            <RouterProvider router={router}></RouterProvider>
+          </HelmetProvider>
+        </QueryClientProvider>
       </AuthProvider>
     </>
   );
