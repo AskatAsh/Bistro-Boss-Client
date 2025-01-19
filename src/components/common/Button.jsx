fcb9d08488773a9ac6e63d48d9ecacc55d5de538
AskatAsh <abdulaskat@gmail.com>
2025-01-19 20:05:15 +0600

    added item to cart function using axios post method with swetalert2 to notify success and error

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,9 +1,9 @@
 
 import { PropTypes } from 'prop-types';
 const Button = (props) => {
-    const {btnText = "Click Here", btnStyles = ""} = props;
+    const {btnText = "Click Here", btnStyles = "", btnFunction} = props;
   return (
-    <button className={`btn btn-outline border-0 border-b-[3px] uppercase text-medium ${btnStyles}`}>
+    <button onClick={btnFunction} className={`btn btn-outline border-0 border-b-[3px] uppercase text-medium ${btnStyles}`}>
       {btnText}
     </button>
   );
@@ -12,6 +12,7 @@ const Button = (props) => {
 Button.propTypes = {
     btnText: PropTypes.string,
     btnStyles: PropTypes.string,
+    btnFunction: PropTypes.func,
 }
 
 export default Button;
--- src/components/shop/ItemCard.jsx
+++ src/components/shop/ItemCard.jsx
@@ -1,10 +1,68 @@
+import { useContext } from "react";
 import Button from "../common/Button";
-import { PropTypes } from 'prop-types';
-const ItemCard = ({item}) => {
-  const {image, name, recipe, price} = item;
+import { PropTypes } from "prop-types";
+import { AuthContext } from "../../providers/AuthProvider";
+import axios from "axios";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
+
+const ItemCard = ({ item }) => {
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const { _id, image, name, recipe, price, category } = item;
+
+  const handleAddToCart = () => {
+    const cartItem = {
+      itemId: _id,
+      name,
+      image,
+      recipe,
+      price,
+      userEmail: user?.email,
+      userName: user?.displayName,
+    };
+    if (user && user?.email) {
+      // add item to cart using axios
+      axios
+        .post(`${import.meta.env.VITE_SERVER}/carts`, cartItem)
+        .then((res) => {
+          // console.log(res);
+          if (res?.data?.acknowledged && res?.data?.insertedId) {
+            Swal.fire({
+              title: "Success!",
+              text: "Added to cart successfully!",
+              icon: "success",
+            });
+          }
+        })
+        .catch((error) => {
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: `${error}`,
+          });
+        });
+    } else {
+      Swal.fire({
+        title: "You are not Logged in",
+        text: "Please log in to add items to cart.",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Go to Login"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          navigate('/auth/login', { state: { from: {pathname: `/shop/${category}`} } });
+        }
+      });
+    }
+  };
   return (
     <div className="bg-dark7 relative flex flex-col h-full">
-      <span className="absolute top-3 right-3 bg-footerbg2 font-semibold text-text px-3 py-1 text-sm">${price}</span>
+      <span className="absolute top-3 right-3 bg-footerbg2 font-semibold text-text px-3 py-1 text-sm">
+        ${price}
+      </span>
       <img
         className="w-full max-h-[300px] object-cover"
         src={image}
@@ -12,10 +70,9 @@ const ItemCard = ({item}) => {
       />
       <div className="px-6 lg:px-10 py-4 lg:py-8 flex flex-col flex-1">
         <h3 className="font-semibold text-2xl text-center">{name}</h3>
-        <p className="text-dark3 pt-2 pb-4 text-center flex-grow">
-          {recipe}
-        </p>
+        <p className="text-dark3 pt-2 pb-4 text-center flex-grow">{recipe}</p>
         <Button
+          btnFunction={handleAddToCart}
           btnText="Add to Cart"
           btnStyles="block mx-auto text-golden border-golden bg-dark6 hover:bg-footerbg2"
         />
@@ -26,6 +83,6 @@ const ItemCard = ({item}) => {
 
 ItemCard.propTypes = {
   item: PropTypes.object,
-}
+};
 
 export default ItemCard;
